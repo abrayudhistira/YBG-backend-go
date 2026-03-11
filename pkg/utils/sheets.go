502936afc71e39ed9eed166467febcf13e70ed0b
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-12 02:39:25 +0700

    revert

--- pkg/utils/sheets.go
+++ pkg/utils/sheets.go
@@ -2,10 +2,8 @@ package utils
 
 import (
 	"context"
-	"encoding/json"
 	"io"
 	"net/http"
-	"strings"
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
@@ -20,27 +18,11 @@ func GetSheetsService(ctx context.Context) (*sheets.Service, error) {
 	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	// Baca murni tanpa diubah jadi string atau dimanipulasi
+	credsJSON, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	// 1. Masukkan ke Map dulu supaya format JSON-nya nggak rusak
-	var creds map[string]interface{}
-	if err := json.Unmarshal(body, &creds); err != nil {
-		return nil, err
-	}
-
-	// 2. Ambil private_key dan benerin \n nya di sini
-	if pk, ok := creds["private_key"].(string); ok {
-		creds["private_key"] = strings.ReplaceAll(pk, "\\n", "\n")
-	}
-
-	// 3. Encode balik jadi JSON yang valid
-	finalJSON, err := json.Marshal(creds)
-	if err != nil {
-		return nil, err
-	}
-
-	return sheets.NewService(ctx, option.WithCredentialsJSON(finalJSON))
+	return sheets.NewService(ctx, option.WithCredentialsJSON(credsJSON))
 }
