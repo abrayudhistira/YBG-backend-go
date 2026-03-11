ce26ec6421451e28ee94c2a4360f39e653f69bc1
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-12 02:35:03 +0700

    updated: changes again

--- pkg/utils/sheets.go
+++ pkg/utils/sheets.go
@@ -4,28 +4,30 @@ import (
 	"context"
 	"io"
 	"net/http"
+	"strings"
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
 
 func GetSheetsService(ctx context.Context) (*sheets.Service, error) {
-	// URL Public Supabase kamu
 	fileURL := "https://jmdmommnfxkcyauelsus.supabase.co/storage/v1/object/public/private_assets/service-account.json"
 
-	// 1. Ambil file JSON secara langsung via HTTP
 	resp, err := http.Get(fileURL)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	// 2. Baca seluruh isi body-nya (Raw Bytes)
-	credsJSON, err := io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	// 3. Masukkan ke Google Sheets Service
-	return sheets.NewService(ctx, option.WithCredentialsJSON(credsJSON))
+	// SULAP TERAKHIR:
+	// Mengubah teks literal "\n" menjadi karakter newline asli yang diminta Google
+	content := string(body)
+	content = strings.ReplaceAll(content, "\\n", "\n")
+
+	return sheets.NewService(ctx, option.WithCredentialsJSON([]byte(content)))
 }
