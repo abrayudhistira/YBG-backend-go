eebf0a00fc416401499a99c75f3f6d4fd3cc1741
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-12 02:10:56 +0700

    fixed env issues in utils

--- pkg/utils/sheets.go
+++ pkg/utils/sheets.go
@@ -3,25 +3,22 @@ package utils
 import (
 	"context"
 	"os"
+	"strings" // Tambahkan ini
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
 
-//	func GetSheetsService(ctx context.Context, credentialsPath string) (*sheets.Service, error) {
-//		srv, err := sheets.NewService(ctx, option.WithCredentialsFile(credentialsPath))
-//		if err != nil {
-//			return nil, err
-//		}
-//		return srv, nil
-//	}
 func GetSheetsService(ctx context.Context) (*sheets.Service, error) {
-	// 1. Cek dulu di Environment Variable (Best Practice untuk Cloud)
 	credsJSON := os.Getenv("GOOGLE_CREDENTIALS_JSON")
+
 	if credsJSON != "" {
+		// TRIK SAKTI: Memastikan karakter \n dibaca sebagai baris baru beneran oleh Google
+		credsJSON = strings.ReplaceAll(credsJSON, "\\n", "\n")
+
 		return sheets.NewService(ctx, option.WithCredentialsJSON([]byte(credsJSON)))
 	}
 
-	// 2. Fallback ke file fisik (Hanya untuk kenyamanan saat coding di Laptop/Lokal)
+	// Fallback lokal
 	return sheets.NewService(ctx, option.WithCredentialsFile("config/service-account.json"))
 }
