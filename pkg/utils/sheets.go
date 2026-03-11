e68df2068f7a281f3c37afc814165627482dc440
abrayudhistira <abrayudhistira59@gmail.com>
2026-03-12 02:37:17 +0700

    try: validaion again

--- pkg/utils/sheets.go
+++ pkg/utils/sheets.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"context"
+	"encoding/json"
 	"io"
 	"net/http"
 	"strings"
@@ -24,10 +25,22 @@ func GetSheetsService(ctx context.Context) (*sheets.Service, error) {
 		return nil, err
 	}
 
-	// SULAP TERAKHIR:
-	// Mengubah teks literal "\n" menjadi karakter newline asli yang diminta Google
-	content := string(body)
-	content = strings.ReplaceAll(content, "\\n", "\n")
+	// 1. Masukkan ke Map dulu supaya format JSON-nya nggak rusak
+	var creds map[string]interface{}
+	if err := json.Unmarshal(body, &creds); err != nil {
+		return nil, err
+	}
 
-	return sheets.NewService(ctx, option.WithCredentialsJSON([]byte(content)))
+	// 2. Ambil private_key dan benerin \n nya di sini
+	if pk, ok := creds["private_key"].(string); ok {
+		creds["private_key"] = strings.ReplaceAll(pk, "\\n", "\n")
+	}
+
+	// 3. Encode balik jadi JSON yang valid
+	finalJSON, err := json.Marshal(creds)
+	if err != nil {
+		return nil, err
+	}
+
+	return sheets.NewService(ctx, option.WithCredentialsJSON(finalJSON))
 }
